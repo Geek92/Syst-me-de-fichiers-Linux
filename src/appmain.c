99904d983cc67bcff1aaa59141277e6264e40668
Tchossiewe Djengoue Patrick-frank <patrickfrank.tchossiewedjengoue.etu@b04p10.fil.univ-lille.fr>
2023-01-04 12:50:42 +0100

    exercice 3.2, afficher les valeurs des registres esp et ebp

--- src/appmain.c
+++ src/appmain.c
@@ -2,5 +2,18 @@
 
 void app_main()
 {
-    puts("TODO\n");
+
+
+  int x, y;
+
+  // je copie les valeurs des registres esp et ebp dans x et y
+  asm ("mov %%esp, %1" "\n\t"
+      "mov %%ebp, %0"
+      :"=r"(x),
+       "=r"(y));
+    //j'affiche les valeurs des registres;
+    puthex(x);
+    puts("\n");
+    puthex(y);
+    puts("\n");
 }
