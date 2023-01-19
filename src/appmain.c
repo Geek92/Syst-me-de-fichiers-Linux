b5ebd84d63ec4b171b19c0f5ac9b7b62825a4b09
patrick tchossiewe <patrickfrank.tchossiewedjengoue.etu@univ-lille.fr>
2023-01-19 23:42:39 +0100

    rajout de la fonction switch to context

--- src/appmain.c
+++ src/appmain.c
@@ -1,22 +1,9 @@
 #include "minilib.h"
+#include "appmain.h"
 
 typedef int(func_t)(int);
 typedef void(func_ctx)(void*);
 #define STACK_SIZE 4096
-
-//macro qui permets d'afficher les valeurs des registres esp et ebp de la fonction courante
-#define DUMP_SP() do{  \
-  int esp_value;      \
-  int ebp_value;      \
-                      \
-  asm ("mov %%esp, %0" "\n\t" "mov %%ebp, %1" \
-      :"=r"(esp_value),"=r"(ebp_value));      \
-                                              \
-       putud(esp_value);                      \
-       puts("\n");                            \
-       putud(ebp_value);                      \
-       puts("\n");                            \
-}while(0)
 
 enum state {NEW_CONTEXT, ACTIVE, TERMINATED};
 
@@ -29,10 +16,20 @@ struct pctx {
   char stack[STACK_SIZE];
 };
 
+//declaration des fonctions ping et pong a utiliser pour le changement de contexte
+void f_ping(void *arg);
+void f_pong(void *arg);
+
+//declaration des contextes de  ping et pong
+struct pctx ctx_ping = {.context_state = NEW_CONTEXT};
+struct pctx ctx_pong = {.context_state = NEW_CONTEXT};
 struct pctx save_context;
 
+//contexte courant
+struct pctx *current_context = NULL;
 int saved_r;
-//je definie ma fonction throw
+
+//on definit la fonction throw
 int throw(int r){
   saved_r = r;
   asm("movl %0, %%esp" "\n\t" "movl %1, %%ebp"
@@ -49,7 +46,7 @@ int my_function(int return_value){
   return return_value;
 }
 
-// je definit la function try
+// on definit la function try
 int try(func_t *f, int arg){
   DUMP_SP();
   asm ("mov %%esp, %0" "\n\t" "mov %%ebp, %1"
@@ -57,24 +54,38 @@ int try(func_t *f, int arg){
   return f(arg);
 }
 
-//procedure qui permets d'initialiser un context
+//procedure qui permets d'initialiser un contexte
 int init_ctx(struct pctx *ctx, func_ctx f, void *args){
-  ctx->function_pointer = f;
-  ctx->arg_pointer = args;
-    return 0;
+      ctx->function_pointer = f;
+      ctx->arg_pointer = args;
+      return 0;
 }
+
+//fonction f_ping
+
 
 //procedure qui effectue le changement de contexte
  void switch_to_ctx(struct pctx *ctx){
+    if(current_context == NULL){
 
-
+    } else {
+              SAVE_CONTEXT(current_context);
+              current_context = ctx;
+              LOAD_CONTEXT(current_context);
+              if(current_context->context_state == NEW_CONTEXT){
+                    current_context->context_state = ACTIVE;
+                    current_context->function_pointer(current_context->arg_pointer);
+              }
+    }
  }
 void app_main(){
   //j'appeles ma fonction try
-  int v = try(&my_function,5);
+  /*int v = try(&my_function,5);
   putud(v);
   puts("\n");
   int a = try(&my_function,-2);
-  putud(a);
-
+  putud(a);*/
+      init_ctx(&ctx_ping, f_ping, NULL);
+      init_ctx(&ctx_pong, f_pong, NULL);
+      switch_to_ctx(&ctx_ping);
 }
---
+++ src/appmain.h
@@ -0,0 +1,25 @@
+//macro qui permets d'afficher les valeurs des registres esp et ebp de la fonction courante
+#define DUMP_SP() do{  \
+  int esp_value;      \
+  int ebp_value;      \
+                      \
+  asm ("mov %%esp, %0" "\n\t" "mov %%ebp, %1" \
+      :"=r"(esp_value),"=r"(ebp_value));      \
+                                              \
+       putud(esp_value);                      \
+       puts("\n");                            \
+       putud(ebp_value);                      \
+       puts("\n");                            \
+}while(0)
+
+//macro qui permets de sauvegarder un contexte
+#define SAVE_CONTEXT(current_context){                                     \
+  asm ("mov %0, %%esp" "\n\t" "mov %1,%%ebp"                               \
+      :"=r"(current_context->esp_value),"=r"(current_context->ebp_value)); \
+}
+
+//macro qui permets de charger un contexte
+#define LOAD_CONTEXT(current_context){                                     \
+  asm ("mov %0, %%esp" "\n\t" "mov %1,%%ebp"                              \
+      :"r"(current_context->esp_value),"r"(current_context->ebp_value));   \
+}
